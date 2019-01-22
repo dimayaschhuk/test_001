97a2bd71b83e652d60bb4c9137128cd72735e5d0
dmytro <dimaworkyaschhuk@gmail.com>
2019-01-22 20:03:53 +0200

    fix

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -7,6 +7,11 @@ use Telegram;
 class TelegramController extends Controller
 {
     public function webhook(){
+        $telegramUser=\Telegram::getWebhookUpdates()['message'];
+
+        $this->replyWithMessage(['text'=>'laravel']);
+        $text=sprintf('%s'.PHP_EOL,'test');
+        $this->replyWithMessage(compact('text'));
         Telegram::commandsHandler(TRUE);
     }
 }
