8096171a50ee55391cf9b6760316e7a359660033
dmytro <dimaworkyaschhuk@gmail.com>
2019-01-23 10:31:24 +0200

    fix

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -12,35 +12,49 @@ class TelegramController extends Controller
         $telegramUser=\Telegram::getWebhookUpdates()['message'];
         $text=$telegramUser['text'];
 
-        if($text=='Защита культур'){
+        if(session()->has($telegramUser['from']['id'])){
             $response = \Telegram::sendMessage([
                 'chat_id' => $telegramUser['from']['id'],
-                'text' => 'Введіть назву або перші букви культур',
+                'text' => $telegramUser['from']['id'],
+            ]);
+            $response->getMessageId();
+        }else{
+            session()->put($telegramUser['from']['id'],'вжк є');
+            $response = \Telegram::sendMessage([
+                'chat_id' => $telegramUser['from']['id'],
+                'text' => 'welcome',
             ]);
             $response->getMessageId();
         }
-
-        if($text=='Продукты'){
-            $keyboard = [
-                ['йцу', 'куй'],
-            ];
-
-            $reply_markup = \Telegram::replyKeyboardMarkup([
-                'keyboard' => $keyboard,
-                'resize_keyboard' => true,
-                'one_time_keyboard' => true
-            ]);
-
-            $response = \Telegram::sendMessage([
-                'chat_id' => $telegramUser['from']['id'],
-                'text' => 'Продукты',
-                'reply_markup' => $reply_markup
-            ]);
-            $messageId = $response->getMessageId();
-        }
-        if($text=='/start'){
-//            Telegram::commandsHandler(TRUE);
-        }
+//        if($text=='Защита культур'){
+//            $response = \Telegram::sendMessage([
+//                'chat_id' => $telegramUser['from']['id'],
+//                'text' => 'Введіть назву або перші букви культур',
+//            ]);
+//            $response->getMessageId();
+//        }
+//
+//        if($text=='Продукты'){
+//            $keyboard = [
+//                ['йцу', 'куй'],
+//            ];
+//
+//            $reply_markup = \Telegram::replyKeyboardMarkup([
+//                'keyboard' => $keyboard,
+//                'resize_keyboard' => true,
+//                'one_time_keyboard' => true
+//            ]);
+//
+//            $response = \Telegram::sendMessage([
+//                'chat_id' => $telegramUser['from']['id'],
+//                'text' => 'Продукты',
+//                'reply_markup' => $reply_markup
+//            ]);
+//            $messageId = $response->getMessageId();
+//        }
+//        if($text=='/start'){
+////            Telegram::commandsHandler(TRUE);
+//        }
 
 
 
