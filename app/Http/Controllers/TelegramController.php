8a6d99cc2f6851c47ecda1cb25ff74b29deca234
dmytro <dimaworkyaschhuk@gmail.com>
2019-01-23 16:29:41 +0200

    fix

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -11,24 +11,31 @@ class TelegramController extends Controller
 {
     public function webhook()
     {
-        $request = new Request();
+        $value = session('key');
+
+        // Specifying a default value...
+        $value = session('key', 'default');
+
+        // Store a piece of data in the session...
+        session(['key' => 'value']);
 
         $telegramUser = \Telegram::getWebhookUpdates()['message'];
-        if($request->session()->has($telegramUser['from']['id'])){
-            $response = \Telegram::sendMessage([
-                'chat_id' => $telegramUser['from']['id'],
-                'text' => $request->session()->get($telegramUser['from']['id']),
-            ]);
-            $response->getMessageId();
-        }else{
-
-            $response = \Telegram::sendMessage([
-                'chat_id' => $telegramUser['from']['id'],
-                'text' => 'welcome',
-            ]);
-            $response->getMessageId();
-            $request->session($telegramUser['from']['id'],'вже є');
-        }
+        $response = \Telegram::sendMessage([
+            'chat_id' => $telegramUser['from']['id'],
+            'text' => 'welcome',
+        ]);
+        $response->getMessageId();
+//        if($request->session()->has($telegramUser['from']['id'])){
+//            $response = \Telegram::sendMessage([
+//                'chat_id' => $telegramUser['from']['id'],
+//                'text' => $request->session()->get($telegramUser['from']['id']),
+//            ]);
+//            $response->getMessageId();
+//        }else{
+//
+//
+//            $request->session($telegramUser['from']['id'],'вже є');
+//        }
 //        $response = \Telegram::sendMessage([
 //            'chat_id' => $telegramUser['from']['id'],
 //            'text'    => 'Введіть назву або перші букви культур',
