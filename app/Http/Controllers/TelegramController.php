32ec8f8a5920f5a6f9c79ae208d62237f5b35a8c
dmytro <dimaworkyaschhuk@gmail.com>
2019-01-31 17:08:30 +0200

    fix

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -6,9 +6,11 @@ use App\BaseModels\Culture;
 use App\BaseModels\Problem;
 use App\BaseModels\ProblemGroup;
 use App\BaseModels\Product;
+use App\BaseModels\Technology;
 use App\Commands\SendMessageCommand;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Telegram;
 
 
@@ -174,6 +176,7 @@ class TelegramController extends Controller
 
     public function sendTextProblem($chatId, $text)
     {
+        send_text($chatId, 'sendTextProblem');
         $data = Cache::get($chatId);
         $culture = Culture::find($data['culture_id']);
 
@@ -189,7 +192,7 @@ class TelegramController extends Controller
 
 
         if (empty($culture->getProblemNames($data['problemGroup_id']))) {
-            if($culture->getProblemNames()){
+            if ($culture->getProblemNames()) {
                 send_text($chatId, 'До даної культури немає проблем');
                 $data = Cache::get($chatId);
                 $data['method'] = 'welcome';
@@ -204,12 +207,11 @@ class TelegramController extends Controller
         }
 
 
-
-
     }
 
     public function searchProblem($chatId, $text)
     {
+        send_text($chatId, 'searchProblem');
         $data = Cache::get($chatId);
         $culture = Culture::find($data['culture_id']);
 
@@ -219,6 +221,7 @@ class TelegramController extends Controller
 
     public function selectProblem($chatId, $text)
     {
+        send_text($chatId, 'selectProblem');
         $data = Cache::get($chatId);
         $data['method'] = 'selectProblem';
         $data['problem_id'] = Problem::where('name', $text)->value('id');
@@ -236,7 +239,7 @@ class TelegramController extends Controller
             exit;
         }
         if (empty($culture->getProductsNames($data['problem_id']))) {
-            send_text($chatId,'Препаратів не знайдено виберіть іншу проблему');
+            send_text($chatId, 'Препаратів не знайдено виберіть іншу проблему');
             $this->sendTextProblem($chatId, $text);
         } else {
             $keyboard = get_keyboard($culture->getProductsNames($data['problem_id']));
@@ -250,17 +253,86 @@ class TelegramController extends Controller
         $data['method'] = 'selectProduct';
         $data['product_id'] = Product::where('name', $text)->value('id');
         Cache::put($chatId, $data, self::TIME_CACHE);
-        $keyboard=[
-            ['Применение на культр','Проблематика'],
-            ['Узнать больше','Описание','Цены и наличие']
-            ];
+        $keyboard = [
+            ['Применение на культр', 'Проблематика'],
+            ['Узнать больше', 'Описание', 'Цены и наличие'],
+        ];
         send_keyboard($chatId, $keyboard, 'Що саме вас цікавить?');
+    }
+
+
+    public function sendTextProduct($chatId, $text)
+    {
+        if ($text == 'Применение на культр') {
+            $this->applicationToCulture($chatId, $text);
+        }
+
+        if ($text == 'Проблематика') {
+            $this->getProblem($chatId, $text);
+        }
+
+        if ($text == 'Узнать больше') {
+
+        }
+
+        if ($text == 'Описание') {
+            $this->getDescription($chatId, $text);
+        }
+
+        if ($text == 'Цены и наличие') {
+            $this->getPrice($chatId, $text);
+        }
+
+        $keyboard = [
+            ['Применение на культр', 'Проблематика'],
+            ['Узнать больше', 'Описание', 'Цены и наличие'],
+        ];
+        send_keyboard($chatId, $keyboard, 'Виберіть що саме вас цікавить?');
+    }
+
+
+    public function applicationToCulture($chatId, $text)
+    {
+        $data = Cache::get($chatId);
+        $pd_CultureForCropProcessing = DB::where('cultureId', $data['culture_id'])
+            ->pluck('cropProcessingId')
+            ->toArray();
+        $pd_VerminForCropProcessing = DB::where('verminId', $data['product_id'])
+            ->pluck('cropProcessingId')
+            ->toArray();
+        $technologyIds = array_intersect($pd_CultureForCropProcessing, $pd_VerminForCropProcessing);
+        $technology = Technology::find($technologyIds[0]);
+        $text = "consumptionNormMin:" . $technology->consumptionNormMin . ", consumptionNormMax: " .
+            $technology->consumptionNormMax . ", lastTreatmentTerm: " . $technology->lastTreatmentTerm .
+            ", maxTreatmentCount: " . $technology->maxTreatmentCount . ", experience:" . $technology->experience .
+            ", areaUnit:" . $technology->areaUnit . ", amountUnit:" . $technology->amountUnit;
+        send_text($chatId, $text);
+        $keyboard = [
+            ['Применение на культр', 'Проблематика'],
+            ['Узнать больше', 'Описание', 'Цены и наличие'],
+        ];
+        send_keyboard($chatId, $keyboard, 'Щe щось?');
+
+    }
+
+    public function getProblem($chatId, $text)
+    {
+
+    }
+
+    public function getDescription($chatId, $text)
+    {
+
+    }
+
+    public function getPrice($chatId, $text)
+    {
+
     }
 
 
     public function test($chatId, $text)
     {
-
         $response = \Telegram::sendMessage([
             'chat_id' => $chatId,
             'text'    => 'test: ' . $text,
