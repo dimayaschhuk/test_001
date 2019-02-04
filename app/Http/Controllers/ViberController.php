49cfe99c5268d4e1e48e28ede39ef30d187a6505
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-04 16:25:22 +0200

    test keyboard

--- app/Http/Controllers/ViberController.php
+++ app/Http/Controllers/ViberController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use http\Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Viber\Api\Keyboard;
 use Viber\Client;
 use Viber\Bot;
 use Viber\Api\Sender;
@@ -12,12 +13,13 @@ use Viber\Api\Sender;
 class ViberController extends Controller
 {
 
-    public function setWebhook(){
+    public function setWebhook()
+    {
         $apiKey = '492df57f7927d70b-bb1dffe5ee14eea0-4498222180f6797f';
         $webhookUrl = 'https://www.dimayashchuk.icu/viber_bot'; //
         try {
             dump('try');
-            $client = new Client([ 'token' => $apiKey ]);
+            $client = new Client(['token' => $apiKey]);
             dump($client);
             $result = $client->setWebhook($webhookUrl);
             dd($result);
@@ -25,14 +27,15 @@ class ViberController extends Controller
 
 
         } catch (\ErrorException $e) {
-            echo "Error: ". $e->getError() ."\n";
+            echo "Error: " . $e->getError() . "\n";
         }
     }
 
-    public function webHook(){
+    public function webHook()
+    {
         $apiKey = '492df57f7927d70b-bb1dffe5ee14eea0-4498222180f6797f';
         $botSender = new Sender([
-            'name' => 'mySzrBot',
+            'name'   => 'mySzrBot',
             'avatar' => 'http://chat.organic.mobimill.com/storage/app/public/10/1e7bc03379018d5cfd8a2bb60af3592a.jpg',
         ]);
         try {
@@ -54,13 +57,19 @@ class ViberController extends Controller
                     );
                 })
                 ->onText('|test .*|si', function ($event) use ($bot, $botSender) {
-                    send_text_viber($bot,$botSender,$event,'test');
-//                    $bot->getClient()->sendMessage(
-//                        (new \Viber\Api\Message\Text())
-//                            ->setSender($botSender)
-//                            ->setReceiver($event->getSender()->getId())
-//                            ->setText("test")
-//                    );
+                    send_text_viber($bot, $botSender, $event, 'test');
+
+                })
+                ->onText('|key .*|si', function ($event) use ($bot, $botSender) {
+                    $keyboard = new Keyboard();
+                    $keyboard->setButtons(['test', 'test']);
+                    $bot->getClient()->sendMessage(
+                        (new \Viber\Api\Message\Text())
+                            ->setSender($botSender)
+                            ->setReceiver($event->getSender()->getId())
+                            ->setText("I do not know )")
+                            ->setKeyboard($keyboard)
+                    );
                 })
                 ->run();
         } catch (Exception $e) {
