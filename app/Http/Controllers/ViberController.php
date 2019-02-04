f07129f9ff23b585bd22cda162b671ac2b09fd7c
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-04 11:50:20 +0200

    fix

--- app/Http/Controllers/ViberController.php
+++ app/Http/Controllers/ViberController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use http\Exception;
 use Illuminate\Http\Request;
 use Viber\Client;
+use Viber\Bot;
+use Viber\Api\Sender;
 
 class ViberController extends Controller
 {
@@ -12,15 +15,48 @@ class ViberController extends Controller
         $apiKey = '492df57f7927d70b-bb1dffe5ee14eea0-4498222180f6797f';
         $webhookUrl = 'https://www.dimayashchuk.icu/viber_bot'; //
         try {
+            dump('try');
             $client = new Client([ 'token' => $apiKey ]);
             $result = $client->setWebhook($webhookUrl);
             echo "Success!\n";
-        } catch (Exception $e) {
+            dd($result);
+
+        } catch (\ErrorException $e) {
             echo "Error: ". $e->getError() ."\n";
         }
     }
 
     public function webHook(){
+        $apiKey = '492df57f7927d70b-bb1dffe5ee14eea0-4498222180f6797f';
 
+        $botSender = new Sender([
+            'name' => 'Whois bot',
+            'avatar' => 'https://developers.viber.com/img/favicon.ico',
+        ]);
+
+        try {
+            $bot = new Bot(['token' => $apiKey]);
+            $bot
+                ->onConversation(function ($event) use ($bot, $botSender) {
+                    // это событие будет вызвано, как только пользователь перейдет в чат
+                    // вы можете отправить "привествие", но не можете посылать более сообщений
+                    return (new \Viber\Api\Message\Text())
+                        ->setSender($botSender)
+                        ->setText("Can i help you?");
+                })
+                ->onText('|whois .*|si', function ($event) use ($bot, $botSender) {
+                    // это событие будет вызвано если пользователь пошлет сообщение
+                    // которое совпадет с регулярным выражением
+                    $bot->getClient()->sendMessage(
+                        (new \Viber\Api\Message\Text())
+                            ->setSender($botSender)
+                            ->setReceiver($event->getSender()->getId())
+                            ->setText("I do not know )")
+                    );
+                })
+                ->run();
+        } catch (Exception $e) {
+            // todo - log exceptions
+        }
     }
 }
