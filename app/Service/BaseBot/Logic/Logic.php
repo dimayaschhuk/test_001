d36009a95b6addd7c5b0c90777e1d3496dab09ca
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-07 15:33:28 +0200

    add traid

--- app/Service/BaseBot/Logic/Logic.php
+++ app/Service/BaseBot/Logic/Logic.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Cache;
 
 class Logic
 {
+    use Methods;
     protected $bot;
 
     const METHOD_WELCOME = 'welcome';
@@ -93,68 +94,6 @@ class Logic
 //
 //            }
         }
-    }
-
-
-    public function welcome()
-    {
-        $this->bot->setText("Виберіть гілку");
-        $this->bot->setKeyboard(['Захист культури', 'Продукти']);
-        $this->bot->send(BaseBot::KEYBOARD);
-        $this->bot->setCurrentMethod(self::METHOD_SELECT_FLOW);
-        exit;
-    }
-
-    public function selectFlow()
-    {
-
-        $flow = $this->bot->getUserText();
-        if ($flow == self::FLOW_PROTECT_CULTURE_UA) {
-            $this->bot->setCurrentFlow(self::FLOW_PROTECT_CULTURE);
-            $this->nextMethod();
-            exit;
-        }
-
-        $this->welcome();
-    }
-
-    public function sendTextCulture()
-    {
-        $this->bot->sendText('Введіть назву культури або перші букви');
-        $this->nextMethod();
-    }
-
-    public function searchCulture()
-    {
-        if (Culture::where('name', $this->bot->getUserText())->count() === 1) {
-            $this->bot->setCurrentMethod(Logic::METHOD_SELECT_CULTURE);
-            $this->bot->sendText('method: METHOD_SELECT_CULTURE');
-            exit;
-        }
-
-        if (Culture::where('name', 'LIKE', "%{$this->bot->getUserText()}%")->count() === 0) {
-            $this->bot->setCurrentMethod(Logic::METHOD_SEND_TEXT_CULTURE);
-            $this->bot->sendText('method: METHOD_SELECT_CULTURE');
-            exit;
-        }
-
-        $this->bot->setText('Виберіть із списка одну культуру яка вам найбільше підходить');
-        $this->bot->setKeyboard(
-            Culture::where('name', 'LIKE', "%{$this->bot->getUserText()}%")
-                ->pluck('name')
-                ->toArray()
-        );
-        $this->bot->send(BaseBot::KEYBOARD);
-    }
-
-
-    public function selectCulture()
-    {
-        $data = Cache::get($chatId);
-        $data['method'] = 'selectCulture';
-        $data['culture_id'] = Culture::where('name', $text)->value('id');
-        Cache::put($chatId, $data, self::TIME_CACHE);
-        $this->sendTextProblemGroup($chatId, $text);
     }
 
 
---
+++ app/Service/BaseBot/Logic/Methods.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmytro
+ * Date: 2/7/19
+ * Time: 3:32 PM
+ */
+
+namespace App\Service\BaseBot\Logic;
+
+
+trait Methods
+{
+    public function welcome()
+    {
+        $this->bot->setText("Виберіть гілку");
+        $this->bot->setKeyboard(['Захист культури', 'Продукти']);
+        $this->bot->send(BaseBot::KEYBOARD);
+        $this->bot->setCurrentMethod(self::METHOD_SELECT_FLOW);
+        exit;
+    }
+
+    public function selectFlow()
+    {
+
+        $flow = $this->bot->getUserText();
+        if ($flow == self::FLOW_PROTECT_CULTURE_UA) {
+            $this->bot->setCurrentFlow(self::FLOW_PROTECT_CULTURE);
+            $this->nextMethod();
+            exit;
+        }
+
+        $this->welcome();
+    }
+
+    public function sendTextCulture()
+    {
+        $this->bot->sendText('Введіть назву культури або перші букви');
+        $this->nextMethod();
+    }
+
+    public function searchCulture()
+    {
+        if (Culture::where('name', $this->bot->getUserText())->count() === 1) {
+            $this->bot->setCurrentMethod(Logic::METHOD_SELECT_CULTURE);
+            $this->bot->sendText('method: METHOD_SELECT_CULTURE');
+            exit;
+        }
+
+        if (Culture::where('name', 'LIKE', "%{$this->bot->getUserText()}%")->count() === 0) {
+            $this->bot->setCurrentMethod(Logic::METHOD_SEND_TEXT_CULTURE);
+            $this->bot->sendText('method: METHOD_SELECT_CULTURE');
+            exit;
+        }
+
+        $this->bot->setText('Виберіть із списка одну культуру яка вам найбільше підходить');
+        $this->bot->setKeyboard(
+            Culture::where('name', 'LIKE', "%{$this->bot->getUserText()}%")
+                ->pluck('name')
+                ->toArray()
+        );
+        $this->bot->send(BaseBot::KEYBOARD);
+    }
+
+
+    public function selectCulture()
+    {
+        $data = Cache::get($chatId);
+        $data['method'] = 'selectCulture';
+        $data['culture_id'] = Culture::where('name', $text)->value('id');
+        Cache::put($chatId, $data, self::TIME_CACHE);
+        $this->sendTextProblemGroup($chatId, $text);
+    }
+
+}
