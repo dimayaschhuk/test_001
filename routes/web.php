cc5f61e630dc90c1c7f97e7ea7869e87bc35b15d
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-07 12:01:42 +0200

    web

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+use App\Service\BaseBot\BaseBot;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Viber\Api\Keyboard;
@@ -34,118 +35,29 @@ Route::get('/setWebHook', 'ViberController@setWebhook');
 Route::get('/set', 'TestController@setWebHook');
 Route::get('/get', 'TestController@getWebHookInfo');
 
-Route::get('/testViber', function () {
+Route::get('/deleteMyCacheViber', function () {
     {
-        $apiKey = '492df57f7927d70b-bb1dffe5ee14eea0-4498222180f6797f';
-        $botSender = new Sender([
-            'name'   => 'mySzrBot',
-            'avatar' => 'http://chat.organic.mobimill.com/storage/app/public/10/1e7bc03379018d5cfd8a2bb60af3592a.jpg',
-        ]);
-        $button = new \Viber\Api\Keyboard\Button();
-        $button->setColumns(1);
-        $button->setRows(1);
-        $button->setBgColor('#fff');
-        $button->setText('ds');
-        $keyboard = new Keyboard();
-        $keyboard->setBgColor('#fff');
-        $keyboard->setDefaultHeight(TRUE);
-        $keyboard->setButtons([$button]);
-        dd($keyboard);
-            $bot = new Bot(['token' => $apiKey]);
-
-            $bot->onConversation(function ($event) use ($bot, $botSender) {
-                    return (new \Viber\Api\Message\Text())
-                        ->setSender($botSender)
-                        ->setText("Чим я можу вам допомогти?");
-                })
-                ->onText('(.*[а-я,a-z,0-9])', function ($event) use ($bot, $botSender) {
-                    $text = $event->getMessage()->getText();
-                    $button = new \Viber\Api\Keyboard\Button();
-                    $button->setColumns(1);
-                    $button->setRows(1);
-                    $button->setBgColor('#fff');
-                    $button->setActionType('reply');
-                    $button->setActionBody('reply to me');
-                    $button->setText('buttonTest');
-                    $button->setTextSize('regular');
-                    $keyboard = new Keyboard();
-                    $keyboard->setBgColor('#fff');
-                    $keyboard->setButtons([$button]);
-
-                    $bot->getClient()->sendMessage(
-                        (new \Viber\Api\Message\Text())
-                            ->setKeyboard($keyboard)
-                            ->setSender($botSender)
-                            ->setReceiver($event->getSender()->getId())
-                            ->setText("342")
-                    );
-
-
-                    $bot->getClient()->sendMessage(
-                        (new \Viber\Api\Message\Text())
-                            ->setKeyboard($keyboard)
-                            ->setSender($botSender)
-                            ->setReceiver($event->getSender()->getId())
-                            ->setText("342")
-                    );
-                })
-
-                ->run();
-//        } catch (Exception $e) {
-//            dd('Exception',$e);
-//            Log::info('not send message');
-//        }
-
-//        "receiver":"01234567890A=",
-//   "type":"text",
-//   "text":"Hello world",
-//   "keyboard":{
-//          "Type":"keyboard",
-//          "DefaultHeight":true,
-//          "Buttons":[
-//              {
-//                  "ActionType":"reply",
-//                  "ActionBody":"reply to me",
-//                  "Text":"Key text",
-//                  "TextSize":"regular"
-//         }
-//      ]
-//   }
+        Cache::pull(BaseBot::TYPE_VIBER."/"."cT0AJq4mBsVbUX1ITQRd4w==");
+        dd('delete viber...done');
     }
 });
 
-
-Route::get('/getCache', function () {
+Route::get('/deleteMyCacheTelegram', function () {
     {
-        dd(Cache::get('test'));
+        Cache::pull(BaseBot::TYPE_TELGRAM."/"."563738410");
+        dd('delete telegram...done');
     }
 });
 
-Route::get('/getMyCache', function () {
+Route::get('/getMyCacheTelegram', function () {
     {
-        dd(Cache::get(563738410));
+
+        dd(Cache::get(BaseBot::TYPE_TELGRAM."/"."563738410"));
     }
 });
 
-Route::get('/resetMyCache', function () {
+Route::get('/getMyCacheViber', function () {
     {
-        $value = ['flow' => 'testFlow', 'method' => 'welcome'];
-        Cache::put(563738410, $value, 1);
-        dd(Cache::get(563738410));
-    }
-});
-
-
-
-Route::get('/setCacheTest', function () {
-    {
-        Cache::put('qwe', 'dssds', 1);
-        dd(Cache::get('qwe'));
-    }
-});
-
-Route::get('/getCacheTest', function () {
-    {
-        dd(Cache::get('qwe'));
+        dd(Cache::get(BaseBot::TYPE_VIBER."/"."cT0AJq4mBsVbUX1ITQRd4w=="));
     }
 });
