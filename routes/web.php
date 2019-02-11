3202464394a713191f2706ecb389ee50de465a04
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-11 10:45:20 +0200

    fix sendTextProblem

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,11 @@
 |
 */
 
+use App\BaseModels\Culture;
+use App\BaseModels\Problem;
+use App\BaseModels\ProblemGroup;
 use App\Service\BaseBot\BaseBot;
+use App\Service\BaseBot\Logic\Logic;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Viber\Api\Keyboard;
@@ -79,24 +83,90 @@ Route::get('/getMyCacheViber', function () {
 });
 
 
+Route::get('/testMyViberBot', function () {
+    {
+        $text='1111';
+        $chatId="cT0AJq4mBsVbUX1ITQRd4w==";
+        $baseBot = new BaseBot(BaseBot::TYPE_VIBER, $chatId);
+        $baseBot->setUserText($text);
+        $baseBot->runMethod();
+        dd('testMyViberBot');
+    }
+});
+
+Route::get('/testMyTelegramBot', function () {
+    {
+        $text='1111';
+        $chatId="563738410";
+        $baseBot = new BaseBot(BaseBot::TYPE_TELGRAM, $chatId);
+        $baseBot->setUserText($text);
+        $baseBot->runMethod();
+        dd('testMyTelegramBot');
+    }
+});
+
+
 Route::get('/testBot', function () {
     {
-        $chatId = 111111;
-        $baseBot = new BaseBot(BaseBot::TYPE_TELGRAM, $chatId);
-        $baseBot->setCurrentFlow(\App\Service\BaseBot\Logic\Logic::FLOW_PROTECT_CULTURE);
-        $logic = new \App\Service\BaseBot\Logic\Logic($baseBot);
+        $culture = Culture::find(233);
+        $userText = 'Парша';
 
-
-        try {
-            $key = array_search($baseBot->currentFlow, $logic->getMethod()[$baseBot->currentFlow]);
-            $baseBot->setCurrentMethod($this->getMethod()[$baseBot->currentFlow][$key + 1]);
+        if ($culture->checkProblem($userText)) {
+           dd('stop');
             exit;
-        } catch (ErrorException $errorException) {
-            if (empty($baseBot->currentFlow)) {
-                $this->bot->setCurrentMethod($this->getMethod()[$this->bot->currentFlow][0]);
+        }
+        dd('dd');
+        if (empty($culture->getProblemNames($this->bot->getProblemGroupId()))) {
+            if ($culture->getProblemNames()) {
+                $this->bot->sentText('До даної культури немає проблем');
+                $this->bot->setCurrentMethod(Logic::METHOD_SEND_TEXT_CULTURE);
+                $this->sendTextCulture();
                 exit;
             }
+            $this->bot->sentText('До даної культури не знайдено проблеми з цієї групи проблем');
+            $this->sendTextProblemGroup();
+            exit;
+        } else {
+            $this->bot->setKeyboard($culture->getProblemNames($this->bot->getProblemGroupId()));
+            $this->bot->setText('Виберіть назву проблеми');
+            $this->bot->send(BaseBot::KEYBOARD);
         }
+
 
     }
 });
+
+Route::get('/MIGRATE', function () {
+    {
+        $dir = '../data/dampDB';
+        $files = scandir($dir);
+
+        unset($files[0]);
+        unset($files[1]);
+
+        foreach ($files as $file) {
+            $filename = $dir . "/" . $file;
+            $templine = '';
+            $lines = file($filename);
+            foreach ($lines as $line) {
+
+                if (substr($line, 0, 2) == '--' || $line == '') {
+                    continue;
+                }
+
+                $templine .= $line;
+
+                if (substr(trim($line), -1, 1) == ';') {
+
+                    \Illuminate\Support\Facades\DB::select($templine);
+//                mysqli_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysqli_error() . '<br /><br />');
+                    $templine = '';
+                }
+            }
+        }
+
+        echo "Tables imported successfully";
+    }
+});
+
+
