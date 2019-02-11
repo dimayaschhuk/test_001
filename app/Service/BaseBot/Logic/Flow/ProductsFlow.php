45bb03a638d6a5d9ccb3090083337b0949e6690b
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-11 17:45:25 +0200

    fix

--- app/Service/BaseBot/Logic/Flow/ProductsFlow.php
+++ app/Service/BaseBot/Logic/Flow/ProductsFlow.php
@@ -17,139 +17,145 @@ use App\Service\BaseBot\Logic\Logic;
 trait ProductsFlow
 {
 
-//    public function Pr_sendTextProductGroup()
-//    {
-//        $this->bot->setCurrentMethod(Logic::METHOD_PR_SEND_TEXT_PRODUCT_GROUP);
-//        $userText = $this->bot->getUserText();
-//
-//        $currentPage = $this->bot->getCurrentPageProductGroup();
-//        $currentPage = ($userText != Logic::BUTTON_BACK) ? $currentPage : (($currentPage > 1) ? --$currentPage : $currentPage);
-//        $currentPage = ($userText == Logic::BUTTON_FORWARD) ? ++$currentPage : $currentPage;
-//        $this->bot->setCurrentPageProductGroup($currentPage);
-//
-//        $product = Product::where("name", $userText)->first();
-//        if (!empty($product)) {
-//            $this->bot->setProductId($product->id);
-//            $this->afterSelectedProduct();
-//            exit;
-//        }
-//        $product = Product::where("name", "LIKE", "{$userText}%")->get();
-//        if (!$product->isEmpty() && $product->count() < 15) {
-//            $this->Pr_searchProducts();
-//            exit;
-//        }
-//
-//        $productGroup = ProductGroup::where('name', $userText)->first();
-//        if (!empty($productGroup)) {
-//            $this->bot->setProductGroupId($productGroup->id);
-//            $this->Pr_sendTextBrand();
-//            exit;
-//        }
-//
-//        $this->bot->setText('Виберіть із списку групу в яку входить препарат');
-//        $prodGroupNames = ProductGroup::where("name", "LIKE", "{$userText}%")->limit(14)->pluck('name')->toArray();
-//
-//        if (empty($prodGroupNames)) {
-//            $this->bot->setText('Введіть перші 3 букви назви препарату або виберіть із списку групу в яку входить препарат');
-//            $prodGroupNames = ProductGroup::offset(($currentPage - 1) * 15)->limit($currentPage * 15)->pluck('name')->toArray();
-//
-//            if (!empty($prodGroupNames)) {
-//                $prodGroupNames[] = Logic::BUTTON_FORWARD;
-//            }
-//            $prodGroupNames[] = Logic::BUTTON_BACK;
-//        } else {
-//            $prodGroupNames[] = Logic::BUTTON_ALL_PRODUCT_GROUP;
-//        }
-//
-//        $this->bot->setKeyboard($prodGroupNames);
-//        $this->bot->send(BaseBot::KEYBOARD);
-//    }
+    public function Pr_sendTextProductGroup()
+    {
+        $this->bot->setCurrentMethod(Logic::METHOD_PR_SEND_TEXT_PRODUCT_GROUP);
+        $userText = $this->bot->getUserText();
 
-//    public function Pr_searchProducts()
-//    {
-//        $userText = $this->bot->getUserText();
-//        $productNames = Product::where("name", "LIKE", "{$userText}%")
-//            ->limit(15)
-//            ->pluck('name')
-//            ->toArray();
-//        $this->bot->setText('Виберіть із списку препарат');
-//
-//        $this->bot->setKeyboard($productNames);
-//        $this->bot->send(BaseBot::KEYBOARD);
-//
-//    }
+        $currentPage = $this->bot->getCurrentPageProductGroup();
+        $currentPage = ($userText != Logic::BUTTON_BACK) ? $currentPage : (($currentPage > 1) ? --$currentPage : $currentPage);
+        $currentPage = ($userText == Logic::BUTTON_FORWARD) ? ++$currentPage : $currentPage;
+        $this->bot->setCurrentPageProductGroup($currentPage);
 
-//    public function Pr_sendTextBrand()
-//    {
-//        $this->bot->setCurrentMethod(Logic::METHOD_PR_SEND_TEXT_BRAND);
-//        $productGroup = ProductGroup::find($this->bot->getProductGroupId());
-//        $userText = $this->bot->getUserText();
-//
-//
-//        $band = Brand::where('name', $userText)->first();
-//        if (!empty($band)) {
-//            $this->bot->sendText('!empty($band)');
-//            $this->bot->setBrandId($band->id);
-//            $this->Pr_sendTextProducts();
-//        }
-//        $this->bot->sendText('dsfsdfsd');
-//        if (empty($productGroup->getBrandNames())) {
-//            $this->bot->sendText('Брендів до цієї групи не знайдено');
-//            $this->Pr_sendTextProductGroup();
-//        } else {
-//            $this->bot->setText('Виберіть із списку бренд');
-//            $this->bot->setKeyboard($productGroup->getBrandNames());
-//            $this->bot->send(BaseBot::KEYBOARD);
-//        }
-//
-//
-//    }
+        $product = Product::where("name", $userText)->first();
+        if (!empty($product)) {
+            $this->bot->setProductId($product->id);
+            $this->afterSelectedProduct();
+            exit;
+        }
+
+        $product = Product::where("name", "LIKE", "{$userText}%")->get();
+        if (!$product->isEmpty() && $product->count() < 15) {
+            $this->Pr_searchProducts();
+            exit;
+        }
+
+        $productGroup = ProductGroup::where('name', $userText)->first();
+        if (!empty($productGroup)) {
+            $this->bot->setProductGroupId($productGroup->id);
+            $this->Pr_sendTextBrand();
+            exit;
+        }
+
+        $this->bot->setText('Виберіть із списку групу в яку входить препарат');
+        $prodGroupNames = ProductGroup::where("name", "LIKE", "{$userText}%")->limit(14)->pluck('name')->toArray();
+
+        if (empty($prodGroupNames)) {
+            $this->bot->setText('Введіть перші 3 букви назви препарату або виберіть із списку групу в яку входить препарат');
+            $prodGroupNames = ProductGroup::offset(($currentPage - 1) * 15)->limit($currentPage * 15)->pluck('name')->toArray();
+
+            if (!empty($prodGroupNames)) {
+                $prodGroupNames[] = Logic::BUTTON_FORWARD;
+            }
+            $prodGroupNames[] = Logic::BUTTON_BACK;
+        } else {
+            $prodGroupNames[] = Logic::BUTTON_ALL_PRODUCT_GROUP;
+        }
+
+        $this->bot->setKeyboard($prodGroupNames);
+        $this->bot->send(BaseBot::KEYBOARD);
+    }
+
+    public function Pr_searchProducts()
+    {
+        $userText = $this->bot->getUserText();
+        $productNames = Product::where("name", "LIKE", "{$userText}%")
+            ->limit(15)
+            ->pluck('name')
+            ->toArray();
+        $this->bot->setText('Виберіть із списку препарат');
+
+        $this->bot->setKeyboard($productNames);
+        $this->bot->send(BaseBot::KEYBOARD);
+
+    }
+
+    public function Pr_sendTextBrand()
+    {
+        $this->bot->setCurrentMethod(Logic::METHOD_PR_SEND_TEXT_BRAND);
+        $productGroup = ProductGroup::find($this->bot->getProductGroupId());
+        $userText = $this->bot->getUserText();
 
 
-//    public function Pr_sendTextProducts()
-//    {
-//        $this->bot->setCurrentMethod(Logic::METHOD_PR_SEND_TEXT_PRODUCT);
-//        $userText = $this->bot->getUserText();
-//        $productGroupId = $this->bot->getProductGroupId();
-//        $brandId = $this->bot->getBrandId();
-//        $currentPage = $this->bot->getCurrentPageProduct();
-//        $currentPage = ($userText != Logic::BUTTON_BACK) ? $currentPage : (($currentPage > 1) ? --$currentPage : $currentPage);
-//        $currentPage = ($userText == Logic::BUTTON_FORWARD) ? ++$currentPage : $currentPage;
-//        $this->bot->setCurrentPageProduct($currentPage);
-//
-//        $productNames = Product::where("name", "LIKE", "{$userText}%")
-//            ->where('brandId', $brandId)
-//            ->limit(12)
-//            ->pluck('name')
-//            ->toArray();
-//
-//        $product = Product::where("name", $userText)->where('brandId', $brandId)->first();
-//        if (!$product->isEmpty()) {
-//            $this->bot->setProductId($product->id);
-//            $this->afterSelectedProduct();
-//        }
-//
-//        if (empty($productNames)) {
-//            $productNames = Product::where('groupId', $productGroupId)
-//                ->where('brandId', $brandId)
-//                ->offset(($currentPage - 1) * 15)->limit($currentPage * 15)
-//                ->pluck('name')->toArray();
-//
-//            if (!empty($productNames)) {
-//                $productNames[] = Logic::BUTTON_FORWARD;
-//            }
-//            $productNames[] = Logic::BUTTON_BACK;
-//        } else {
-//            $productNames[] = Logic::BUTTON_ALL_PRODUCT;
-//        }
-//
-//
-//        $this->bot->setText('Виберіть із списку препарат');
-//
-//        $this->bot->setKeyboard($productNames);
-//        $this->bot->send(BaseBot::KEYBOARD);
-//
-//    }
+        $band = Brand::where('name', $userText)->first();
+        if (!empty($band)) {
+            $this->bot->setBrandId($band->id);
+            $this->Pr_sendTextProducts();
+            exit;
+        }
+
+        if (empty($productGroup->getBrandNames())) {
+            $this->bot->sendText('Брендів до цієї групи не знайдено');
+            $this->Pr_sendTextProductGroup();
+            exit;
+        } else {
+            $this->bot->setText('Виберіть із списку бренд');
+            $this->bot->setKeyboard($productGroup->getBrandNames());
+            $this->bot->send(BaseBot::KEYBOARD);
+            exit;
+        }
+
+
+    }
+
+
+    public function Pr_sendTextProducts()
+    {
+        $this->bot->sendText('Pr_sendTextProducts');
+        exit;
+
+        $this->bot->setCurrentMethod(Logic::METHOD_PR_SEND_TEXT_PRODUCT);
+        $userText = $this->bot->getUserText();
+        $productGroupId = $this->bot->getProductGroupId();
+        $brandId = $this->bot->getBrandId();
+        $currentPage = $this->bot->getCurrentPageProduct();
+        $currentPage = ($userText != Logic::BUTTON_BACK) ? $currentPage : (($currentPage > 1) ? --$currentPage : $currentPage);
+        $currentPage = ($userText == Logic::BUTTON_FORWARD) ? ++$currentPage : $currentPage;
+        $this->bot->setCurrentPageProduct($currentPage);
+
+        $productNames = Product::where("name", "LIKE", "{$userText}%")
+            ->where('brandId', $brandId)
+            ->limit(12)
+            ->pluck('name')
+            ->toArray();
+
+        $product = Product::where("name", $userText)->where('brandId', $brandId)->first();
+        if (!$product->isEmpty()) {
+            $this->bot->setProductId($product->id);
+            $this->afterSelectedProduct();
+        }
+
+        if (empty($productNames)) {
+            $productNames = Product::where('groupId', $productGroupId)
+                ->where('brandId', $brandId)
+                ->offset(($currentPage - 1) * 15)->limit($currentPage * 15)
+                ->pluck('name')->toArray();
+
+            if (!empty($productNames)) {
+                $productNames[] = Logic::BUTTON_FORWARD;
+            }
+            $productNames[] = Logic::BUTTON_BACK;
+        } else {
+            $productNames[] = Logic::BUTTON_ALL_PRODUCT;
+        }
+
+
+        $this->bot->setText('Виберіть із списку препарат');
+
+        $this->bot->setKeyboard($productNames);
+        $this->bot->send(BaseBot::KEYBOARD);
+
+    }
 
 
 }
