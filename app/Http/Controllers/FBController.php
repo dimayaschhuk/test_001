049a03e22724243ec30b74f63f10b276eab19cce
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-21 13:24:05 +0200

    add FBController

--- app/Http/Controllers/FBController.php
+++ app/Http/Controllers/FBController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use pimax\FbBotApp;
 
 class FBController extends Controller
 {
-    public function webHook(){
-        dd('hello');
+    public function webHook()
+    {
+        $verify_token = "bb041372d8aa65d06c854cc2060f36f9"; // Verify token
+        if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) {
+            echo $_REQUEST['hub_challenge'];
+        }
     }
 }
