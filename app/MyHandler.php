9dfef1263566dd76691ca6ef7cd331ac0cdba354
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-23 19:46:58 +0200

    test button

--- app/MyHandler.php
+++ app/MyHandler.php
@@ -16,6 +16,7 @@ use Casperlaitw\LaravelFbMessenger\Messages\Text;
 
 class MyHandler extends BaseHandler
 {
+    protected $payload = 'USER_DEFINED_PAYLOAD';
 
     public function handle(ReceiveMessage $message)
     {
