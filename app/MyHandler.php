ebe50a31d9e9b8bb2db411834cb8ac230367138f
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-23 19:58:52 +0200

    test button

--- app/MyHandler.php
+++ app/MyHandler.php
@@ -22,31 +22,31 @@ class MyHandler extends BaseHandler
 
     public function handle(ReceiveMessage $message)
     {
-//        $button = new ButtonTemplate($message->getSender(), 'Default text');
-//        $button
-//            ->setText('Choose')
-//            ->addPostBackButton('First Bbutton')
-//            ->addPostBackButton('Second Button')
-//            ->addPostBackButton('Third button');
-//        $this->send($button);
+        $button = new ButtonTemplate($message->getSender(), 'Default text');
+        $button
+            ->setText('Choose')
+            ->addPostBackButton('First Bbutton')
+            ->addPostBackButton('Second Button')
+            ->addPostBackButton('Third button');
+        $this->send($button);
 
         $text = new Text($message->getSender(), "Default Handler: {$message->getMessage()}");
         $text
             ->addQuick(new QuickReply('1', '1'))
             ->addQuick(new QuickReply('2', '2'))
-            ->addQuick(new QuickReply('3', '3'))
-            ->addQuick(new QuickReply('4', '4'))
-            ->addQuick(new QuickReply('5', '5'))
-            ->addQuick(new QuickReply('6', '6'))
-            ->addQuick(new QuickReply('7', '7'))
-            ->addQuick(new QuickReply('8', '8'))
-            ->addQuick(new QuickReply('9', '9'))
-            ->addQuick(new QuickReply('10','10'))
-            ->addQuick(new QuickReply('11','11'))
-            ->addQuick(new QuickReply('12','12'))
-            ->addQuick(new QuickReply('13','13'))
-            ->addQuick(new QuickReply('14','14'))
-            ->addQuick(new QuickReply('15','15'));
+            ->addQuick(new QuickReply('3', '3'));
+//            ->addQuick(new QuickReply('4', '4'))
+//            ->addQuick(new QuickReply('5', '5'))
+//            ->addQuick(new QuickReply('6', '6'))
+//            ->addQuick(new QuickReply('7', '7'))
+//            ->addQuick(new QuickReply('8', '8'))
+//            ->addQuick(new QuickReply('9', '9'))
+//            ->addQuick(new QuickReply('10','10'))
+//            ->addQuick(new QuickReply('11','11'))
+//            ->addQuick(new QuickReply('12','12'))
+//            ->addQuick(new QuickReply('13','13'))
+//            ->addQuick(new QuickReply('14','14'))
+//            ->addQuick(new QuickReply('15','15'));
 
 
         $this->send($text);
