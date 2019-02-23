9db18184903481008bf022779e254687584e41c8
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-23 19:51:56 +0200

    test button

--- app/MyHandler.php
+++ app/MyHandler.php
@@ -11,6 +11,7 @@ namespace App;
 
 use Casperlaitw\LaravelFbMessenger\Contracts\BaseHandler;
 use Casperlaitw\LaravelFbMessenger\Messages\ButtonTemplate;
+use Casperlaitw\LaravelFbMessenger\Messages\QuickReply;
 use Casperlaitw\LaravelFbMessenger\Messages\ReceiveMessage;
 use Casperlaitw\LaravelFbMessenger\Messages\Text;
 
@@ -21,12 +22,18 @@ class MyHandler extends BaseHandler
 
     public function handle(ReceiveMessage $message)
     {
-        $button = new ButtonTemplate($message->getSender(), 'Default text');
-        $button
-            ->setText('Choose')
-            ->addPostBackButton('First Bbutton')
-            ->addPostBackButton('Second Button')
-            ->addPostBackButton('Third button');
-        $this->send($button);
+//        $button = new ButtonTemplate($message->getSender(), 'Default text');
+//        $button
+//            ->setText('Choose')
+//            ->addPostBackButton('First Bbutton')
+//            ->addPostBackButton('Second Button')
+//            ->addPostBackButton('Third button');
+//        $this->send($button);
+
+        $text = new Text($message->getSender(), "Default Handler: {$message->getMessage()}");
+        $text->addQuick(new QuickReply('Red', 'PAYLOAD_RED'))
+            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'));
+
+        $this->send($text);
     }
 }
