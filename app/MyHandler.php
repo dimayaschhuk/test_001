2fb179a46e2f8a918edaf878615197731b193177
dmytro <dimaworkyaschhuk@gmail.com>
2019-02-23 19:57:21 +0200

    test button

--- app/MyHandler.php
+++ app/MyHandler.php
@@ -31,33 +31,23 @@ class MyHandler extends BaseHandler
 //        $this->send($button);
 
         $text = new Text($message->getSender(), "Default Handler: {$message->getMessage()}");
-        $text->addQuick(new QuickReply('Red', 'PAYLOAD_RED'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'))
+        $text
+            ->addQuick(new QuickReply('1', '1'))
+            ->addQuick(new QuickReply('2', '2'))
+            ->addQuick(new QuickReply('3', '3'))
+            ->addQuick(new QuickReply('4', '4'))
+            ->addQuick(new QuickReply('5', '5'))
+            ->addQuick(new QuickReply('6', '6'))
+            ->addQuick(new QuickReply('7', '7'))
+            ->addQuick(new QuickReply('8', '8'))
+            ->addQuick(new QuickReply('9', '9'))
+            ->addQuick(new QuickReply('10','10'))
+            ->addQuick(new QuickReply('11','11'))
+            ->addQuick(new QuickReply('12','12'))
+            ->addQuick(new QuickReply('13','13'))
+            ->addQuick(new QuickReply('14','14'))
+            ->addQuick(new QuickReply('15','15'));
 
-            ->addQuick(new QuickReply('Green', 'PAYLOAD_GREEN'));
 
         $this->send($text);
     }
